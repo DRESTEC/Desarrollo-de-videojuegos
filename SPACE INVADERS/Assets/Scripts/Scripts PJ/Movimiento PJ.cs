481824def0019a236e4da681611b91b0b5d96ae2
DRESTEC <diego.vivas.2020@upb.edu.co>
2024-10-03 11:26:54 -0500

    Animación eje x

--- SPACE INVADERS/Assets/Scripts/Scripts PJ/Movimiento PJ.cs
+++ SPACE INVADERS/Assets/Scripts/Scripts PJ/Movimiento PJ.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class MovimientoPJ : MonoBehaviour
 {
     [SerializeField] private float Velocidad;
+
     private Rigidbody2D Movimiento;
+    private Animator animator;
+    private SpriteRenderer spritePJ;
     
 
     private void Awake()
     {
         Movimiento = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+        spritePJ = GetComponentInChildren<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -19,5 +24,15 @@ public class MovimientoPJ : MonoBehaviour
         float Vertical = Input.GetAxis("Vertical");
 
         Movimiento.velocity = new Vector2(Horizontal, Vertical)*Velocidad;
+        animator.SetFloat("Camina", Mathf.Abs(Movimiento.velocity.magnitude));
+
+        if (Horizontal > 0)
+        {
+            spritePJ.flipX = false;
+        }
+        else if (Horizontal < 0)
+        {
+            spritePJ.flipX = true;
+        }
     }
 }
