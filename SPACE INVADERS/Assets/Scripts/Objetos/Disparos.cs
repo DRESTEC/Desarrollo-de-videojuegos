c25323e1887fb93d7e55c2c7066b28e9743507c8
DRESTEC <diego.vivas.2020@upb.edu.co>
2024-10-23 13:27:26 -0500

    Sonido disparo
    Se asigno un sonido de disparo al arma

--- SPACE INVADERS/Assets/Scripts/Objetos/Disparos.cs
+++ SPACE INVADERS/Assets/Scripts/Objetos/Disparos.cs
@@ -6,20 +6,27 @@ public class Disparos : MonoBehaviour
 {
     [SerializeField] private Transform origenBalas;
     [SerializeField] private GameObject bala;
-    
+    [SerializeField] private AudioSource audioSource; // Referencia al AudioSource
+    [SerializeField] private AudioClip sonidoDisparo; // Clip de sonido del disparo
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")){
+        if (Input.GetButtonDown("Fire1"))
+        {
             Disparar();
         }
-        
     }
-    private void Disparar(){
+
+    private void Disparar()
+    {
         Quaternion rotacionBala = origenBalas.rotation * Quaternion.Euler(0, 0, -90); // Corrige 90 grados si está mal orientada
         Instantiate(bala, origenBalas.position, rotacionBala);
 
-         
+        // Reproducir el sonido del disparo
+        if (audioSource != null && sonidoDisparo != null)
+        {
+            audioSource.PlayOneShot(sonidoDisparo); // Reproduce el sonido del disparo
+        }
     }
 }
